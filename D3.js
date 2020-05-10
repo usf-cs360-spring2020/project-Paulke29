6e84b625f33d948df65d9c16a14cd64bfd0fed52
Paul Ke <paulke29@yahoo.com>
2020-05-10 15:27:46 -0400

    color legend done

--- D3.js
+++ D3.js
@@ -2,9 +2,18 @@ const urls = {
   basemap: "https://data.sfgov.org/resource/xfcw-9evu.geojson",
   streets: "https://data.sfgov.org/resource/3psu-pn9h.geojson?$limit=20000",
 };
+var margin = {top: 30, right: 30, bottom: 30, left: 30},
+  width = 1100
+  height =600;
 
 
 const svg = d3.select("body").select("svg#vis");
+const Legend = d3.select("body").select("div#legend").append("svg")
+  .attr("width", width)
+  .attr("height", 75)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
 const g = {
   basemap: svg.select("g#basemap"),
@@ -13,6 +22,10 @@ const g = {
   tooltip: svg.select("g#tooltip"),
   details: svg.select("g#details")
 };
+let scale = {};
+scale.color = d3.scaleLinear()
+    .range(["white", "red"])
+    .domain([1,700])
 
 // setup tooltip (shows neighborhood name)
 const tip = g.tooltip.append("text").attr("id", "tooltip");
@@ -21,9 +34,7 @@ tip.attr("dx", -5);
 tip.attr("dy", -5);
 tip.style("visibility", "hidden");
 
-var margin = {top: 30, right: 30, bottom: 30, left: 30},
-  width = 1100
-  height =600;
+
 
 // append the svg object to the body of the page
 var svg2 = d3.select("#my_dataviz")
@@ -66,27 +77,6 @@ svg2.append("g")
   .attr("id","axiY")
   .attr("transform", "translate("+10+"0)")
   .call(d3.axisLeft(y));
-   //
-   // var x2;
-   // var y2
-   // var x2 = d3.scaleBand()
-  //   .range([ 0, 300])
-  //   .domain(myGroups)
-  //   .padding(0.01);
-  // chart2.append("g")
-  //   .attr("transform", "translate("+75+"0)")
-  //   // .attr("display","none")
-  //   .call(d3.axisTop(x2))
-  //
-  // // Build X scales and axis:
-  // var y2 = d3.scaleBand()
-  //   .range([ 200, 0 ])
-  //   .domain(myVars)
-  //   .padding(0.01);
-  // chart2.append("g")
-  //   .attr("transform", "translate("+75+"0)")
-  //   .call(d3.axisLeft(y2));
-
 
   // Build color scale
 var myColor = d3.scaleLinear()
@@ -155,7 +145,7 @@ function Ncharts(data,name,newGroup){
   }
   return result;
 }
-d3.csv(csv).then(drawHeatMap)
+d3.csv(csv).then(drawHeatMap).then(drawColorLegend)
 function drawHeatMap(data) {
     const groupData = DefaultChart(data);
     console.log("Goup: "+JSON.stringify(groupData))
@@ -168,9 +158,73 @@ function drawHeatMap(data) {
         .attr("transform", "translate("+10+"0)")
         .attr("width", x.bandwidth() )
         .attr("height", y.bandwidth() )
-        .style("fill", function(d) { return myColor(d.value)} )
+        .style("fill", function(d) { return scale.color(d.value)} )
 
  }
+ function drawColorLegend() {
+        const legendWidth = 200;
+        const legendHeight = 20;
+
+        // place legend in its own group
+        const group = Legend.append("g");
+        group.attr('id', 'color-legend');
+
+        // shift legend to appropriate position
+        group.attr('transform', "translate("+85+"0)");
+
+        const title = group.append('text')
+          .attr('class', 'axis-title')
+          .text('Average Daily Count of Incidents');
+          title.attr('dx', -39);
+          title.attr('dy', 12);
+
+        const legendLabelMin = group.append('text')
+          .text('0')
+          .attr('dx', -45)
+          .attr('dy', 35);
+
+        const legendLabelMax = group.append('text')
+          .text('700')
+          .attr('dx', 185)
+          .attr('dy', 35);
+
+        // lets draw the rectangle, but it won't have a fill just yet
+        const colorbox = group.append('rect')
+          .attr('x', -17)
+          .attr('y', 12 + 6)
+          .attr('width', legendWidth)
+          .attr('height', legendHeight);
+
+        // get the domain first (we do not want the middle value from the diverging scale)
+        // .range(["white", "red"])
+        // .domain([1,700])
+        const colorDomain = [d3.min(scale.color.domain()), d3.max(scale.color.domain())];
+
+        // add a new scale to go from color tick to percent
+        scale.percent = d3.scaleLinear()
+          .range([0, 100])
+          .domain(colorDomain);
+
+        // we have to first add gradients
+        const defs = Legend.append('defs');
+
+        // add a color stop per data tick
+        // input  (ticks)   : [0.2, ..., 100, ..., 200.3]
+        // output (percents): [  0, ..., 50, ..., 100]
+        defs.append('linearGradient')
+          .attr('id', 'gradient')
+          .selectAll('stop')
+          .data(scale.color.ticks())
+          .enter()
+          .append('stop')
+          .attr('offset', d => scale.percent(d) + '%')
+          .attr('stop-color', d => scale.color(d));
+
+        // draw the color rectangle with the gradient
+        colorbox.attr('fill', 'url(#gradient)');
+
+
+      }
  function drawNchart(data,Name){
    const groupData = Ncharts(data,Name);
    console.log("Goup2: "+JSON.stringify(groupData))
